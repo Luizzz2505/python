96b513cdc57cb630350bf0ab3e737abfbf8b1084
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-17 13:30:39 -0300

    Add files via upload

---
+++ curso_python/aula012.py
@@ -0,0 +1,11 @@
+# exercício
+
+nome = 'Luiz Francisco'
+altura = 1.70
+peso = 80
+imc = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'de altura', 'pesa', peso, 'quilos', 'e seu IMC é', imc )
+
+# ellipsis server para deixar no lugar que você ainda está pensando no código
+...
