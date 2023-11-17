2228e8221505ccd792256aa466e9a58e205ba5a1
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-17 09:05:17 -0300

    aula001.py

--- curso_python/aula001.py
+++ curso_python/aula001.py
@@ -1,4 +1,13 @@
-# Permite escrever um comentário
-print(123)
+# Permite escrever um comentário 
+
+# função print exige algo na tela
+print(123) # do lado
+# abaixo
 print(456)
 
+"""
+
+aqui é uma Docstring.  (não é um comentário e o interpretador do python lê e guarda na memória) 
+Permitindo que eu comente no espaço que estiver entre as aspas.
+
+"""
