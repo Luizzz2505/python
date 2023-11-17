4a284561ef80928d379f09a04e2ed483c660999f
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-17 10:04:54 -0300

    Update aula004.py

--- curso_python/aula004.py
+++ curso_python/aula004.py
@@ -1,23 +1,22 @@
-'''''''''
-Tipos int e float
-
-int -> números inteiros
-float -> números com vírgula
-
-o int representa qualquer número, seja ele negativo ou positivo
-o float representa qualquer número com ponto flutuante, ou seja, números quebrados (vírgula)
-
-'''''''''
+# tipos int e float
+# int -> número inteiro
+# o tipo int representa qualquer número
+# positivo ou negativo. int sem sinal é considerado positivo
 
 print(11) # int
 print(-11) # int
 print(0)
-print(1.1, 10.11) # float
-print(0.0, -1.5)
-
-# A função type mostra o tipo de que Python inferiu ao valor
-print(type('Luiz'))
-print(type(10))
-print(type(10.12))
 
 
+# float -> número com ponto flutuante 
+# o tipo float representa qualquer número positivo ou negativo
+# float sem sinal é considerado positivo 
+
+print(1.1, 10.11) # para separar números SEMPRE se usa um ponto 
+print(0.0) # float
+print(-1.5) # float 
+
+# a função type mostra o tipo que o python inferiu ao valor
+print(type('Hello, World')) # aqui ao invés de imprimir minha string, vai mostrar que tipo de dado ela é
+print(type(123))
+print(type(1.1))
