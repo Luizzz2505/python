bce32a9bfc99a37bc4792fba7c7cbc0084ef18b5
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-17 12:50:29 -0300

    Update aula010.py

--- curso_python/aula010.py
+++ curso_python/aula010.py
@@ -1,8 +1,9 @@
-concatenacao = 'Luiz' + str(1) + ' ' + 'Francisco'
-print(concatenacao)
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao) # juntou todos as strings 
+# o + faz a adição, mas dependendo do tipo ele faz a concatenação (polimorfismo)
 
-a_dez_vezes = 'A' * 10 # repetição
-tres_vezes_luiz = 3 * 'Luiz'
+# o * além da multiplicação, também serve para fazer a repetição
+a_dez_vezes = 'A' * 10
 print(a_dez_vezes)
+tres_vezes_luiz = 3 * 'Luiz'
 print(tres_vezes_luiz)
-print( 10, 10, 10, sep='-')
