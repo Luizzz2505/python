b3909853180235155bdd529da60e3487cf52da31
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-17 09:22:12 -0300

    Update aula002.py

--- curso_python/aula002.py
+++ curso_python/aula002.py
@@ -1,4 +1,12 @@
-print(12, 34, sep=',')
-print(56, 78, sep=',', end='##\n')
-print(1234, 34, 1, sep='-')
+# é usada para imprimir coisas na tela
+# elas recebem argumentos
+print(12, 34) # a vírgula por padrão cria um espaço no terminal
+print(56, 76) # cada print é uma linha nova no terminal
 
+# \r\n -> CRLF (quebra de linha)
+print(12, 34, 1011, sep='-') # o sep faz com que o separador dos argumentos (nesse caso não nomeados) seja substituídos pelo que você colocar
+print(56, 78, sep=".")
+
+#\n também serve para quebrar a linha. Pode ser usado em alguns caso, se for preciso
+print(987, 765, sep='-', end='##') ## o end faz com que no lugar da quebra de linha fique o caracter que eu mandei 
+print(777)
