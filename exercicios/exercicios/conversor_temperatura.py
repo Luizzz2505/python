a31c91acfdf1cf0bf93e86e1b5b197e4dac023c9
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-17 19:34:05 -0300

    Delete exercicios/exercicios directory

--- exercicios/exercicios/conversor_temperatura.py
+++
@@ -1,7 +0,0 @@
-# Crie um programa que converta a temperatura de Celsius para Fahrenheit. Solicite a temperatura em Celsius do usuário e exiba o resultado usando f-strings.
-
-temperatura_c = float(input('Informe a temperatura me graus Celsius: '))
-temperatura_f = 9/5 * temperatura_c + 32
-
-print(f'A temperatua que você informou em Celsius é: {temperatura_f} em Fahrenheit ')
-
--- exercicios/exercicios/media.py
+++
@@ -1,9 +0,0 @@
-# Crie um programa que solicite ao usuário as notas de três disciplinas e calcule a média. Exiba o resultado formatado usando f-strings.
-
-nota1 = float(input('Informe sua primeira nota: '))
-nota2 = float(input('Informe sua segunda nota: '))
-nota3 = float(input('Informe sua terceira nota: '))
-
-media = (nota1 + nota2 + nota3) / 3
-
-print(f'Sua média foi de {media}')
