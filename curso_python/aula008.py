3e2719940d64f698029bc299c0c6ce577aeaa6d2
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-17 12:18:02 -0300

    Update aula008.py

--- curso_python/aula008.py
+++ curso_python/aula008.py
@@ -1,14 +1,15 @@
-nome = 'Luiz'
-sobrenome =  'francisco'
-idade = 19
-ano_nascimento = 2023 - idade
-maior_de_idade = idade >= 18
-altura_metros = 1.70
+# exercício sobre variáveis
 
+nome = 'Luiz'
+sobrenome = 'Francisco'
+idade = 19
+ano_nascimento = 2004
+maior_de_idade = 2023 - ano_nascimento >= 18
+altura_metros = 1.70
 
 print('Nome:', nome)
 print('Sobrenome:', sobrenome)
-print('Idade:', idade) 
+print('Idade:', idade)
 print('Ano de nascimento:', ano_nascimento)
-print('É maior de idade:', maior_de_idade)
+print('É maior de idade?', maior_de_idade)
 print('Altura em metros:', altura_metros)
