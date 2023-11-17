bf8fcab13ceffb5c54d0cfdb56e873e9b48b2969
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-17 13:07:32 -0300

    Add files via upload

---
+++ curso_python/aula011.py
@@ -0,0 +1,15 @@
+# 1. (n + n) (os parênteses são feitos de dentro para fora)
+# 2. **
+# 3. * / // %
+# 4. + -
+
+conta_1 = 1 + 1 ** 5 + 5  # 7
+print(conta_1)
+
+conta_2 = (1 + 1) ** (5 + 5)  # 1024
+print(conta_2) 
+
+# se eu criar uma variável numa linha e depois definir outro valor a ela, vai ficar definido que o valor correto é o mais atual
+nome = 'Luiz'
+nome = 19
+print(nome)
