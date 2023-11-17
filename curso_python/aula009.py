baff7031303bbd7cc9991f3499848b7751097716
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-17 12:37:57 -0300

    Update aula009.py

--- curso_python/aula009.py
+++ curso_python/aula009.py
@@ -1,24 +1,20 @@
 adicao = 10 + 10
-print('Adição', adicao)
+print('Adição:', adicao)
 
 subtracao = 10 - 5
-print('Subtração', subtracao)
+print('Subtração:', subtracao)
 
 multiplicacao = 10 * 10
-print('Multiplicação', multiplicacao)
+print('Multiplicação:', multiplicacao)
 
-divisao = 10 / 2.2  # float
-print("Divisão", divisao)
+divisao = 10 / 2.2 # float
+print('Divisão:', divisao)
 
-divisao_inteira = 10 // 2.2 
-print("Divisão inteira", divisao_inteira)
+divisao_inteira = 10 // 2.2  # com as duas barras os resultados não vão retornar com as casas decimais 
+print('Divisão inteira:', divisao_inteira)
 
 exponenciacao = 2 ** 10
-print('Exponenciação', exponenciacao)
+print('Exponencição', exponenciacao)
 
 modulo = 55 % 2 # resto da divisão
-print('Módulo', modulo)
-
-print(10 % 3 == 0)
-print(10 % 2 == 0)
-
+print("Módulo:", modulo)
