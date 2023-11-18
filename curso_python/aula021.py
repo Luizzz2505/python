34208fc029d7688727a7eb31ae2383a7862804c6
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2023-11-18 15:51:59 -0300

    Add files via upload

---
+++ curso_python/aula021.py
@@ -0,0 +1,18 @@
+# operadores lógicos
+# and (e) or (ou) not (não)
+# and - todas as condições precisam ser verdadeiras
+# se qualquer valor for considerado falso, a expressão inteira será avaliada naquele valor
+# são considerados falsy (para aquela condição é falsa, mas para outras não necessariamente é falsa também) (que você já viu)
+# 0 0.0 '' False
+# também existe o tipo None que é usado para representar um não valor
+
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '123456'
+
+if entrada == 'E' and senha_digitada == senha_permitida: # esse bloco do if só será executado se toda a expressão for verdadeira (por causa do and)
+    print('Entrar')
+else:  # como eu não usei o elif, qualquer outra coisa diferente de E que for digitado vai para o bloco do else (diferente de S também)
+    print('Sair')
+
