eaf61d6d4cc2bae3a770b0a3f9d4bb09cb469168
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2024-02-07 19:31:37 -0300

    Add files via upload

---
+++ exercicios/somar.py
@@ -0,0 +1,11 @@
+# Faça um loop para somar até 100
+
+entrada = int(input('Você quer que o programa conte até que número?: '))
+pular = int(input('De quanto em quanto?: '))
+
+valor = 0 
+
+while valor <= entrada:
+    print(valor)
+    valor += pular
+
