b9edd56c5eb5856590a98b17bef4a187d129c99d
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2024-02-08 16:00:14 -0300

    Add files via upload

--- curso_python/aula034.py
+++ curso_python/aula034.py
@@ -7,8 +7,20 @@ Loop infinito -> Quando um código não tem fim
 
 contador = 0
 
-while contador <= 10: 
+while contador <= 100: 
     contador += 1
+
+    if contador == 6:
+        print('Não vou mostrar o 6')
+        continue # quando o leitor chega no continue ele volta para o começo do laço
+
+    if contador >= 10 and contador <= 27:
+        print('Você aqui não')
+        continue
+
     print(contador)
+
+    if contador == 40:
+        break
 
 print('Acabou') 
